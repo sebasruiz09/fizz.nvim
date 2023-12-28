083cfc28c5d707125c6cee6357e6f04203be74c8
sebasruiz09 <sebasruiz909@gmail.com>
2023-12-27 19:22:09 -0500

    FIX : delete colors for white background

--- lua/lualine/themes/fizz.lua
+++ lua/lualine/themes/fizz.lua
@@ -1,33 +1,21 @@
--- Copyright (c) 2020-2021 Mofiqul Islam
--- MIT license, see LICENSE for more details.
+-- @github.com/sebasruiz09
 local fizz = {}
-local colors = {}
 
-if vim.o.background == "dark" then
-    colors.bg = "#262626"
-    colors.bg2 = "#373737"
-    colors.fg = "#ffffff"
-    colors.red = "#f44747"
-    colors.green = "#4EC9B0"
-    colors.blue = "#0a7aca"
-    colors.lightblue = "#5CB6F8"
-    colors.yellow = "#ffaf00"
-    colors.pink = "#DDB6F2"
-else
-    colors.bg = "#F5F5F5"
-    colors.bg2 = "#E4E4E4"
-    colors.fg = "#343434"
-    colors.red = "#FF0000"
-    colors.green = "#008000"
-    colors.blue = "#AF00DB"
-    colors.lightblue = "#0451A5"
-    colors.yellow = "#ffaf00"
-    colors.pink = "#FFA3A3"
-end
+local colors = {
+    bg = "#262626",
+    bg2 = "#373737",
+    fg = "#ffffff",
+    red = "#f44747",
+    green = "#4EC9B0",
+    blue = "#0a7aca",
+    lightblue = "#5CB6F8",
+    yellow = "#ffaf00",
+    pink = "#DDB6F2",
+}
 
 fizz.normal = {
     b = { fg = colors.blue, bg = colors.bg2 },
-    a = { fg = vim.o.background == "dark" and colors.fg or colors.bg, bg = colors.blue, gui = "bold" },
+    a = { fg = colors.fg, bg = colors.blue, gui = "bold" },
     c = { fg = colors.fg, bg = colors.bg },
 }
 
@@ -43,19 +31,19 @@ fizz.inactive = {
 
 fizz.replace = {
     b = { fg = colors.red, bg = colors.bg2 },
-    a = { fg = vim.o.background == "dark" and colors.bg or colors.fg, bg = colors.red, gui = "bold" },
+    a = { fg = colors.bg, bg = colors.red, gui = "bold" },
     c = { fg = colors.fg, bg = colors.bg },
 }
 
 fizz.insert = {
-    a = { fg = vim.o.background == "dark" and colors.bg or colors.fg, bg = colors.green, gui = "bold" },
+    a = { fg = colors.bg, bg = colors.green, gui = "bold" },
     b = { fg = colors.green, bg = colors.bg2 },
     c = { fg = colors.fg, bg = colors.bg },
 }
 
 fizz.command = {
     b = { fg = colors.pink, bg = colors.bg2 },
-    a = { fg = vim.o.background == "dark" and colors.bg or colors.fg, bg = colors.pink, gui = "bold" },
+    a = { fg = colors.bg, bg = colors.pink, gui = "bold" },
     c = { fg = colors.fg, bg = colors.bg },
 }
 
